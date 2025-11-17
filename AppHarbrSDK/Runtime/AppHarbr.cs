7024da97579d12cf021600f27d8a2321d76c3100
appharbr <ido.ozdova@appharbr.com>
2025-11-17 18:09:14 +0200

    1.18.4

--- AppHarbrSDK/Runtime/AppHarbr.cs
+++ AppHarbrSDK/Runtime/AppHarbr.cs
@@ -16,7 +16,7 @@ public class AppHarbr :
 #endif
 {
     public static AHSdkConfiguration SdkConfiguration = null;
-    private const string version = "1.18.1";
+    private const string version = "1.18.4";
 
     /// <summary>
     /// Gets the current AppHarbr plugin version.
