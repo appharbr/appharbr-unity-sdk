801c361e8e336f775065fb55d0223d26d64b7815
appharbr <ido.ozdova@appharbr.com>
2025-11-18 15:08:29 +0200

    Using newer versions of Android and iOS

--- AppHarbrSDK/Runtime/AppHarbr.cs
+++ AppHarbrSDK/Runtime/AppHarbr.cs
@@ -16,7 +16,7 @@ public class AppHarbr :
 #endif
 {
     public static AHSdkConfiguration SdkConfiguration = null;
-    private const string version = "1.18.8";
+    private const string version = "1.18.9";
 
     /// <summary>
     /// Gets the current AppHarbr plugin version.
