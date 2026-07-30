fa457b6f6b889bf4bd80ad39dfbd16cc545cfd89
Parag Bafna <parag.bafna@geoedge.com>
2026-07-30 15:11:27 +0530

    Update AppHarbr Unity plugin version to 1.24.0

--- AppHarbrSDK/Runtime/AppHarbr.cs
+++ AppHarbrSDK/Runtime/AppHarbr.cs
@@ -16,7 +16,7 @@ public class AppHarbr :
 #endif
 {
     public static AHSdkConfiguration SdkConfiguration = null;
-    private const string version = "1.23.0";
+    private const string version = "1.24.0";
 
     /// <summary>
     /// Gets the current AppHarbr plugin version.
